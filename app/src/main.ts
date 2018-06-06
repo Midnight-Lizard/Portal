e134dbfa23a61cc907be24458f9c5b6eb40bf525
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-06-06 10:08:10 +0300

    switching from "asp.net" to "ng cli+nodejs"

---
+++ app/src/main.ts
@@ -0,0 +1,33 @@
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+import { hmrBootstrap } from './hmr';
+
+if (environment.production)
+{
+  enableProdMode();
+}
+
+const bootstrap = () => platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch(err => console.log(err) as any);
+
+
+if (environment.hmr)
+{
+  if (module['hot'])
+  {
+    hmrBootstrap(module, bootstrap);
+  }
+  else
+  {
+    console.error('HMR is not enabled for webpack-dev-server!');
+  }
+}
+else
+{
+  bootstrap();
+}
