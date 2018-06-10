b5e8a4354618aae2799c2183c0562491b016a993
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-06-10 11:05:20 +0300

    adding store

---
+++ app/src/app/store/app.state.ts
@@ -0,0 +1,38 @@
+﻿import { User } from 'oidc-client';
+import { ActionFakeTypes, Action } from './app.action-sets';
+
+
+export const AppFeature: keyof RootState = 'ML';
+
+export interface ActionError
+{
+    readonly errorMessage?: string;
+    readonly originalError: any;
+    readonly source: ActionFakeTypes | Action;
+}
+
+export interface AppState
+{
+    user?: User;
+    returnUrl: string;
+}
+
+export interface RootState
+{
+    ML: AppState;
+}
+
+export const initialState: RootState = { ML: { returnUrl: '/' } };
+
+export function loadInitialState(): RootState
+{
+    if (typeof document === 'object')
+    {
+        const mlState = document.querySelector('ml-state');
+        if (mlState && mlState.textContent)
+        {
+            return JSON.parse(mlState.textContent);
+        }
+    }
+    return initialState;
+}
