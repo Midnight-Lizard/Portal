8f67cd92c045760e34dd4b1caaf8766ad605f46a
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-06-11 16:50:50 +0300

    ngrx dev tools

--- app/src/app/app.browser.module.ts
+++ app/src/app/app.browser.module.ts
@@ -1,17 +1,23 @@
-import { BrowserModule, BrowserTransferStateModule, TransferState } from '@angular/platform-browser';
+import { BrowserTransferStateModule, TransferState } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { INITIAL_STATE } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './components/app/app.component';
 import { AppSharedModule } from './app.shared.module';
 import { loadInitialState } from './store/app.state';
+import { environment } from '../environments/environment';
 
 @NgModule({
   imports: [
     BrowserAnimationsModule,
     BrowserTransferStateModule,
     AppSharedModule,
+    StoreDevtoolsModule.instrument({
+      maxAge: environment.production ? 5 : 100,
+      logOnly: environment.production
+    })
   ],
   providers: [
     {
