c1b617e02602502ae0b3619290a124f5ed6cd63b
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-07-13 16:19:08 +0300

    core lib tests

--- app/projects/core/src/lib/settings/settings.service.spec.ts
+++ app/projects/core/src/lib/settings/settings.service.spec.ts
@@ -1,15 +1,24 @@
 import { TestBed, inject } from '@angular/core/testing';
+import { TransferState } from '@angular/platform-browser';
 
 import { SettingsService } from './settings.service';
+import { defaultSettings } from './settings';
 
-describe('SettingsService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [SettingsService]
+describe('SettingsService', () =>
+{
+    beforeEach(() =>
+    {
+        TestBed.configureTestingModule({
+            providers: [SettingsService,
+                {
+                    provide: TransferState, useValue: new Map<string, any>([
+                        ['settings', defaultSettings]])
+                }]
+        });
     });
-  });
 
-  it('should be created', inject([SettingsService], (service: SettingsService) => {
-    expect(service).toBeTruthy();
-  }));
+    it('should be created', inject([SettingsService], (service: SettingsService) =>
+    {
+        expect(service).toBeTruthy();
+    }));
 });
