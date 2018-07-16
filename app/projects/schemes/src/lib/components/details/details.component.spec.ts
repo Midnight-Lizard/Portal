63d0df884d98d62d62cb05f8aad0283d4ae9c182
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-07-16 17:18:43 +0300

    loading display tests

--- app/projects/schemes/src/lib/components/details/details.component.spec.ts
+++ app/projects/schemes/src/lib/components/details/details.component.spec.ts
@@ -11,8 +11,7 @@ import * as Act from '../../store/schemes.actions';
 import { SchemesRootState } from '../../store/schemes.state';
 import { PublicSchemeDetails } from '../../model/public-scheme';
 
-describe(nameOfClass(SchemeDetailsComponent), function (
-    this: { scheme: PublicSchemeDetails })
+describe(nameOfClass(SchemeDetailsComponent), function ()
 {
     let component: SchemeDetailsComponent;
     let fixture: ComponentFixture<SchemeDetailsComponent>;
@@ -33,44 +32,67 @@ describe(nameOfClass(SchemeDetailsComponent), function (
         fixture.detectChanges();
     });
 
-    beforeEach(inject([Store, SchemesService],
-        (store$: Store<SchemesRootState>, schemesService: SchemesService) =>
-        {
-            schemesService
-                .getPublicSchemeDetails('test')
-                .subscribe(result =>
-                {
-                    this.scheme = result;
-                    store$.dispatch(new Act.CurrentSchemeChanged({ currentScheme: result }));
-                });
-            TestSchedulerStub.flush();
-            fixture.detectChanges();
-            spyOn(store$, 'dispatch');
-        }));
 
-    it(`should dispatch LikeScheme or DislikeScheme action on click`, inject(
+    describe('After Scheme loaded', function (this: { scheme: PublicSchemeDetails })
+    {
+        beforeEach(inject([Store, SchemesService],
+            (store$: Store<SchemesRootState>, schemesService: SchemesService) =>
+            {
+                schemesService
+                    .getPublicSchemeDetails('test')
+                    .subscribe(result =>
+                    {
+                        this.scheme = result;
+                        store$.dispatch(new Act.CurrentSchemeChanged({ currentScheme: result }));
+                    });
+                TestSchedulerStub.flush();
+                fixture.detectChanges();
+                spyOn(store$, 'dispatch');
+            }));
+
+        it(`should dispatch LikeScheme or DislikeScheme action on click`, inject(
+            [Store], (store$: Store<SchemesRootState>) =>
+            {
+                const likeButton = fixture.debugElement
+                    .queryAll(By.directive(MatButton))[0];
+                click(likeButton);
+                fixture.detectChanges();
+                const expectedAction = this.scheme.liked
+                    ? new Act.DislikeScheme(this.scheme)
+                    : new Act.LikeScheme(this.scheme);
+                expect(store$.dispatch).toHaveBeenCalledWith(expectedAction);
+            }));
+
+        it(`should dispatch correct Favorites management action on click`, inject(
+            [Store], (store$: Store<SchemesRootState>) =>
+            {
+                const favoritesButton = fixture.debugElement
+                    .queryAll(By.directive(MatButton))[1];
+                click(favoritesButton);
+                fixture.detectChanges();
+                const expectedAction = this.scheme.favorited
+                    ? new Act.RemoveSchemeFromFavorites(this.scheme)
+                    : new Act.AddSchemeToFavorites(this.scheme);
+                expect(store$.dispatch).toHaveBeenCalledWith(expectedAction);
+            }));
+
+        it(`should not display loading when there is a scheme`, inject(
+            [Store], (store$: Store<SchemesRootState>) =>
+            {
+                expectLoadingElementDisplayToBe(fixture, 'none');
+            }));
+    });
+
+    it(`should display loading when there is no scheme`, inject(
         [Store], (store$: Store<SchemesRootState>) =>
         {
-            const likeButton = fixture.debugElement
-                .queryAll(By.directive(MatButton))[0];
-            click(likeButton);
-            fixture.detectChanges();
-            const expectedAction = this.scheme.liked
-                ? new Act.DislikeScheme(this.scheme)
-                : new Act.LikeScheme(this.scheme);
-            expect(store$.dispatch).toHaveBeenCalledWith(expectedAction);
-        }));
-
-    it(`should dispatch correct Favorites management action on click`, inject(
-        [Store], (store$: Store<SchemesRootState>) =>
-        {
-            const favoritesButton = fixture.debugElement
-                .queryAll(By.directive(MatButton))[1];
-            click(favoritesButton);
-            fixture.detectChanges();
-            const expectedAction = this.scheme.favorited
-                ? new Act.RemoveSchemeFromFavorites(this.scheme)
-                : new Act.AddSchemeToFavorites(this.scheme);
-            expect(store$.dispatch).toHaveBeenCalledWith(expectedAction);
+            expectLoadingElementDisplayToBe(fixture, 'block');
         }));
 });
+
+function expectLoadingElementDisplayToBe(fixture: ComponentFixture<SchemeDetailsComponent>, display: string)
+{
+    const loadingElement = fixture.debugElement.query(By.css('common-loading')).nativeElement as HTMLElement;
+    const computedStyle = loadingElement.ownerDocument.defaultView.getComputedStyle(loadingElement);
+    expect(computedStyle.display).toBe(display);
+}
