d2d8635e21e624f8fb03c3b445b7b16bed9c013f
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-07-16 16:53:01 +0300

    serving assets from karma web server

--- app/projects/schemes/karma.conf.js
+++ app/projects/schemes/karma.conf.js
@@ -1,5 +1,6 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+const path = require('path');
 
 module.exports = function (config) {
   config.set({
@@ -14,8 +15,19 @@ module.exports = function (config) {
     ],
     files: [
       "../../node_modules/@angular/material/prebuilt-themes/indigo-pink.css",
-      "../shared/src/styles/material-icons.css"
+      "../shared/src/styles/material-icons.css",
+      {
+        pattern: '../shared/src/assets/*.*',
+        watched: true,
+        included: false,
+        served: true,
+        nocache: true
+      }
     ],
+    proxies: {
+        "/assets/": `/absolute${path.join(config.buildWebpack.root,
+            'projects/shared/src/assets/')}`,
+    },
     client: {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
