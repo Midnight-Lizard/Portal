4c15abf05bc7c93a452ffe7550fa0db113f3e579
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-07-27 14:31:22 +0300

    IDENTITY_PORTAL_CLIENT_SECRET

--- shell/debug-ssr-with-secrets.sh
+++ shell/debug-ssr-with-secrets.sh
@@ -2,6 +2,6 @@
 set -e
 cd "../app" ;
 
-env "PORT=$PORT" "PORTAL_AUTH_SECRET=$(kubectl get secret portal-secret -o jsonpath={.data.auth-secret} | base64 -d)" npm run debug:ssr;
+env "PORT=$PORT" "IDENTITY_PORTAL_CLIENT_SECRET=$(kubectl get secret identity-portal-secret -o jsonpath={.data.auth-secret} | base64 -d)" npm run debug:ssr;
 
 # kubectl get secret portal-secret -o json | jq '.data.\"auth-secret\"' | tr -d '\"' | base64 -d 2>nul
