31e4132d55fc9ca2088dbf8fa4831e02690ffbef
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-07-31 18:44:10 +0300

    displaying beta badges in menu and toolbar

--- app/src/app/components/toolbar/toolbar.component.ts
+++ app/src/app/components/toolbar/toolbar.component.ts
@@ -9,9 +9,9 @@ export class ToolbarComponent
 {
     @Output() sidenavToggleClick = new EventEmitter<never>();
 
-    public readonly items = new Set<{ link: string, icon: string, title: string, tooltip: string, class: string }>([
-        { icon: 'home', link: '/home', title: 'Midnight Lizard', tooltip: 'Home', class: 'home-link mat-button' },
-        { icon: 'palette', link: '/schemes', title: 'SCHEMES', tooltip: 'Color schemes', class: 'normal-link mat-button' },
+    public readonly items = new Set<{ link: string, icon: string, title: string, tooltip: string, class: string, badge?: string }>([
+        { icon: 'home', link: '/home', title: 'Midnight Lizard', tooltip: 'Home', class: 'home-link mat-button', badge: 'beta' },
+        { icon: 'palette', link: '/schemes', title: 'SCHEMES', tooltip: 'Color schemes', class: 'normal-link mat-button', badge: 'beta' },
         // { icon: 'book', link: '/docs', title: 'DOCS', tooltip: 'Documentation', class: 'normal-link mat-button' },
         // { icon: 'bug_report', link: '/issues', title: 'ISSUES', tooltip: 'Bug tracker', class: 'normal-link mat-button' },
         // { icon: 'device_hub', link: '/api', title: 'API', tooltip: 'Commander API', class: 'normal-link mat-button' }
