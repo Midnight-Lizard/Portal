d19d122af7c665c21eb51878a4e114c7300de070
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-08-25 19:26:42 +0300

    dynamic auth for apollo

--- app/projects/schemes/src/lib/backend/schemes.service.ts
+++ app/projects/schemes/src/lib/backend/schemes.service.ts
@@ -4,7 +4,7 @@ import { HttpLink } from 'apollo-angular-link-http';
 import { setContext } from 'apollo-link-context';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { Observable } from 'rxjs';
-import { first, switchMap, map } from 'rxjs/operators';
+import { first, switchMap, map, filter } from 'rxjs/operators';
 
 import { SettingsService, AuthRootState, User } from 'core';
 
@@ -19,58 +19,48 @@ import { SchemesDetailsResult } from '../model/schemes-details-result';
 @Injectable()
 export class SchemesService
 {
-    client: Observable<void>;
-
-
     constructor(
         httpLink: HttpLink,
         store$: Store<AuthRootState>,
         private readonly settingsService: SettingsService,
         private readonly apollo: Apollo)
     {
-        this.client = store$.pipe(
+        const token = store$.pipe(
             select(x => x.AUTH.system),
-            map(system =>
-            {
-                if (system)
-                {
-                    const uri = settingsService.getSettings().SCHEMES_QUERIER_URL.replace(/\/$/, '') + '/query';
-                    const http = httpLink.create({ uri });
-                    const auth = setContext((_, __) => ({
-                        headers: {
-                            Authorization: `Bearer ${system.access_token}`
-                        }
-                    }));
-                    apollo.create({
-                        link: auth.concat(http),
-                        cache: new InMemoryCache()
-                    });
-                }
-            }),
-            first()
-        );
+            filter(x => !!x),
+            map(x => x!.access_token),
+            first());
+        const uri = settingsService.getSettings().SCHEMES_QUERIER_URL.replace(/\/$/, '') + '/query';
+        const http = httpLink.create({ uri });
+        const auth = setContext(async (_, __) => ({
+            headers: {
+                Authorization: `Bearer ${await token.toPromise()}`
+            }
+        }));
+        apollo.create({
+            link: auth.concat(http),
+            cache: new InMemoryCache()
+        });
     }
 
     public getPublicSchemes(filters: SchemesFilters, list: SchemesList, pageSize: number, user?: User | null, cursor?: string | null)
     {
-        return this.client.pipe(switchMap(() =>
-            this.apollo.query<SchemesSearchResults>({
-                query: searchQuery,
-                variables: {
-                    query: filters.query,
-                    side: filters.side,
-                    list, pageSize, cursor,
-                    publisherId: user ? user.claims.sub : null
-                }
-            }).pipe(map(x => x.data.search))));
+        return this.apollo.query<SchemesSearchResults>({
+            query: searchQuery,
+            variables: {
+                query: filters.query,
+                side: filters.side,
+                list, pageSize, cursor,
+                publisherId: user ? user.claims.sub : null
+            }
+        }).pipe(map(x => x.data.search));
     }
 
     public getPublicSchemeDetails(publicSchemeId: PublicSchemeId)
     {
-        return this.client.pipe(switchMap(() =>
-            this.apollo.query<SchemesDetailsResult>({
-                query: detailsQuery,
-                variables: { id: publicSchemeId }
-            }).pipe(map(x => x.data.details))));
+        return this.apollo.query<SchemesDetailsResult>({
+            query: detailsQuery,
+            variables: { id: publicSchemeId }
+        }).pipe(map(x => x.data.details));
     }
 }
