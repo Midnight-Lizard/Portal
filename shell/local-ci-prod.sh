5afc52d3d3a30211caabcbf073305fa21ef25e4c
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-12-01 10:54:01 +0300

    using helm directly

--- shell/local-ci-prod.sh
+++ shell/local-ci-prod.sh
@@ -13,9 +13,10 @@ docker build -t $IMAGE \
 kubectl config use-context minikube
 docker push $IMAGE
 secret=$(echo -n not-a-secret | base64 -w 0);
-./helm-deploy.sh -i $IMAGE -r $PROJ -c ../kube/$PROJ \
+helm upgrade --install --set image=$IMAGE \
     --set env.IDENTITY_URL=http://192.168.1.44:32006/ \
     --set env.PORTAL_URL=http://192.168.1.44:31565/ \
     --set env.SCHEMES_QUERIER_URL=http://192.168.1.44:31005/ \
     --set env.PORTAL_AUTH_REFRESH_INTERVAL=240000 \
     --set secrets.sessionSecret=$secret \
+    $PROJ -c ../kube/$PROJ
