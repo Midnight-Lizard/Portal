805a83e730423cbdad7d98e58d2d4e1790edfe72
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-12-09 09:34:31 +0300

    web-store 'xs' layout fix

--- app/src/app/components/web-store/web-store.component.ts
+++ app/src/app/components/web-store/web-store.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, ViewChildren, QueryList, OnDestroy, ElementRef } from '@angular/core';
+import { MatNavList } from '@angular/material';
+import { ObservableMedia } from '@angular/flex-layout';
+import { Subscription } from 'rxjs';
+
+import { SideService } from 'core';
 
 @Component({
     selector: 'ml-web-store',
     templateUrl: './web-store.component.html',
     styleUrls: ['./web-store.component.scss']
 })
-export class WebStoreComponent {
+export class WebStoreComponent implements OnDestroy
+{
+    private readonly _mediaSub: Subscription;
 
+    @ViewChildren(MatNavList, { read: ElementRef }) navLinks: QueryList<ElementRef>;
+
+    constructor(media: ObservableMedia, env: SideService)
+    {
+        if (env.isBrowserSide)
+        {
+            this._mediaSub = media.subscribe(change =>
+            {
+                const isSmall = change.matches && change.mqAlias === 'xs';
+                if (this.navLinks)
+                {
+                    if (isSmall)
+                    {
+                        this.navLinks.forEach(nl => nl.nativeElement.setAttribute('dense', ''));
+                    }
+                    else
+                    {
+                        this.navLinks.forEach(nl => nl.nativeElement.removeAttribute('dense'));
+                    }
+                }
+            });
+        }
+    }
+
+    ngOnDestroy(): void
+    {
+        if (this._mediaSub)
+        {
+            this._mediaSub.unsubscribe();
+        }
+    }
 }
