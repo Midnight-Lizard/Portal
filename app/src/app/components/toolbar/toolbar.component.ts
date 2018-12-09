daee7a6a15724d4b48e4f0d56169d83d2435ec30
Pavel Agarkov <pavel.agarkov@gmail.com>
2018-12-09 16:32:34 +0300

    adding real landing page

--- app/src/app/components/toolbar/toolbar.component.ts
+++ app/src/app/components/toolbar/toolbar.component.ts
@@ -10,7 +10,7 @@ export class ToolbarComponent
     @Output() sidenavToggleClick = new EventEmitter<never>();
 
     public readonly items = new Set<{ link: string, icon: string, title: string, tooltip: string, class: string, badge?: string }>([
-        { icon: 'home', link: '/home', title: 'Midnight Lizard', tooltip: 'Home', class: 'home-link mat-button', badge: 'beta' },
+        { icon: 'home', link: '/home', title: 'Midnight Lizard', tooltip: 'Home', class: 'home-link mat-button' },
         { icon: 'palette', link: '/schemes', title: 'SCHEMES', tooltip: 'Color schemes', class: 'normal-link mat-button', badge: 'beta' },
         // { icon: 'book', link: '/docs', title: 'DOCS', tooltip: 'Documentation', class: 'normal-link mat-button' },
         // { icon: 'bug_report', link: '/issues', title: 'ISSUES', tooltip: 'Bug tracker', class: 'normal-link mat-button' },
