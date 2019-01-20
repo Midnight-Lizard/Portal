65c2e32d937bc6e5c123b2d034518b131cd194b9
Pavel Agarkov <pavel.agarkov@gmail.com>
2019-01-20 18:40:32 +0300

    using 'color schemes' as title

--- app/src/app/components/toolbar/toolbar.component.ts
+++ app/src/app/components/toolbar/toolbar.component.ts
@@ -9,9 +9,18 @@ export class ToolbarComponent
 {
     @Output() sidenavToggleClick = new EventEmitter<never>();
 
-    public readonly items = new Set<{ link: string, icon: string, title: string, tooltip: string, class: string, badge?: string }>([
-        { icon: 'home', link: '/home', title: 'Midnight Lizard', tooltip: 'Home', class: 'home-link mat-button' },
-        { icon: 'palette', link: '/schemes', title: 'SCHEMES', tooltip: 'Color schemes', class: 'normal-link mat-button', badge: 'beta' },
+    public readonly items = new Set<{
+        link: string, icon: string, class: string, badge?: string,
+        toolbarTitle: string, menuTitle: string, tooltip: string
+    }>([
+        {
+            icon: 'home', link: '/home', class: 'home-link mat-button',
+            toolbarTitle: 'Midnight Lizard', menuTitle: 'Midnight Lizard', tooltip: 'Home'
+        },
+        {
+            icon: 'palette', link: '/schemes', class: 'normal-link mat-button', badge: 'beta',
+            toolbarTitle: 'COLOR SCHEMES', menuTitle: 'Color Schemes', tooltip: 'Color schemes'
+        },
         // { icon: 'book', link: '/docs', title: 'DOCS', tooltip: 'Documentation', class: 'normal-link mat-button' },
         // { icon: 'bug_report', link: '/issues', title: 'ISSUES', tooltip: 'Bug tracker', class: 'normal-link mat-button' },
         // { icon: 'device_hub', link: '/api', title: 'API', tooltip: 'Commander API', class: 'normal-link mat-button' }
