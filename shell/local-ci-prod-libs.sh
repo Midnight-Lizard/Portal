0d453ee2dcc79c845d75122f51c90994c323a848
Pavel Agarkov <pavel.agarkov@gmail.com>
2019-01-22 20:06:43 +0300

    docker multi-stage build angular libs

---
+++ shell/local-ci-prod-libs.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+TAG=$(date +"%Y-%m-%d--%H-%M-%S")
+PROJ=portal
+REGISTRY=localhost:5000
+IMAGE=$REGISTRY/$PROJ:$TAG
+eval $(docker-machine env default --shell bash)
+docker build -t $IMAGE -f ../app/Dockerfile.local ../app
+
+# kubectl config use-context minikube
+# docker push $IMAGE
+# secret=$(echo -n not-a-secret | base64 -w 0);
+# helm upgrade --install --set image=$IMAGE \
+#     --set env.IDENTITY_URL=http://192.168.1.44:32006 \
+#     --set env.PORTAL_URL=http://192.168.1.44:31565 \
+#     --set env.SCHEMES_QUERIER_URL=http://192.168.1.44:31005/ \
+#     --set env.PORTAL_AUTH_REFRESH_INTERVAL=240000 \
+#     --set secrets.sessionSecret=$secret \
+#     $PROJ -c ../kube/$PROJ
